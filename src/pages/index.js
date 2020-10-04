9f5bfc1a90fc8b74c202cc9b6457da71aa9beca4
Jesùs Hernàndez <mjlehz@gmail.com>
2020-10-03 23:59:47 -0500

    completado el inicio y nosotros, con graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/Layout"
 import ImagenHotel from "../components/ImagenHotel"
+import ContenidoInicio from "../components/ContenidoInicio"
 
 const IndexPage = () => (
   <>
     <Layout>
       <ImagenHotel />
+      <ContenidoInicio />
     </Layout>
   </>
 )
--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
+import ContenidoNosotros from "../components/ContenidoNosotros"
 
 const Nosotros = () => {
   return (
     <>
       <Layout>
-        <h1>nosotos</h1>
+        <ContenidoNosotros />
       </Layout>
     </>
   )
